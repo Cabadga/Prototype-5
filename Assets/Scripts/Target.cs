7106dca4d187855de3ab5a8594254d49ff8ff8a7
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-04-05 11:03:31 -0700

    Lesson 2 struggling

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,6 +10,8 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     Vector3 RandomForce()
     {
@@ -40,6 +43,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
