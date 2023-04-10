8481987b7158f594e55948efb0e659da7c9e5545
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-04-10 14:47:40 -0700

    Lesson 3 Step 6 Finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     private float spawnRate = 1.0f;
     private int score;
 
     // Start is called before the first frame update
     void Start()
     {
-       StartCoroutine(SpawnTarget());
+        StartCoroutine(SpawnTarget());
         score = 0;
-       UpdateScore(0);
+        UpdateScore(0);
+        isGameActive = true;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 IEnumerator SpawnTarget()
     {
-        while (true) 
+        while (isGameActive) 
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
